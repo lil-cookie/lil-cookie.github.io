78d9aa38531233120616ec6a7eb22b41b3bddeb3
lil cookie <cookie@ro.ru>
2016-08-27 18:27:49 +0300

    add all files

---
+++ js/arrow.js
@@ -0,0 +1,46 @@
+var updownElem = document.getElementById('updown');
+
+    var pageYLabel = 0;
+
+    updownElem.onclick = function() {
+      var pageY = window.pageYOffset || document.documentElement.scrollTop;
+
+      switch (this.className) {
+        case 'up':
+          pageYLabel = pageY;
+          window.scrollTo(0, 0);
+          this.className = 'down';
+          break;
+
+        case 'down':
+          window.scrollTo(0, pageYLabel);
+          this.className = 'up';
+      }
+
+    }
+
+    window.onscroll = function() {
+      var pageY = window.pageYOffset || document.documentElement.scrollTop;
+      var innerHeight = document.documentElement.clientHeight;
+
+      switch (updownElem.className) {
+        case '':
+          if (pageY > innerHeight) {
+            updownElem.className = 'up';
+          }
+          break;
+
+        case 'up':
+          if (pageY < innerHeight) {
+            updownElem.className = '';
+          }
+          break;
+
+        case 'down':
+          if (pageY > innerHeight) {
+            updownElem.className = 'up';
+          }
+          break;
+
+      }
+    }
